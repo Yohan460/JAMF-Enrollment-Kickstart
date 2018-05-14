04326866dc950e0dac88ba7a60f08bcd546be164
Yohan460 <yoyohan460@gmail.com>
2018-05-13 22:42:33 -0400

    Adding Receipt script

---
+++ Configuration Complete Receipt write.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# Author: Johan McGwire - Yohan @ Macadmins Slack - Johan@McGwire.tech
+#
+# Description: This writes out a Initial Configuration complete receipt
+
+# Writing out a configuration receipt
+touch /Library/InitialConfiguration/.InitialConfigurationComplete
+
+# Exiting and returning the policy call code
+exit $?
